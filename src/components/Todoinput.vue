4f2c3c310d659ab46437ebfc6ac24fbb3e45bfcb
gwkim <kking9444@gmail.com>
2019-11-29 14:37:42 +0900

    Vue Application

--- src/components/Todoinput.vue
+++ src/components/Todoinput.vue
@@ -1,61 +1,72 @@
 <template>
   <div class="inputBox shadow">
     <input type="text" v-model="newTodoItem" placeholder="Type what you have to do" v-on:keyup.enter="addTodo">
-    <span class="addContainer" v-on:keyup.enter="addTodo">
-    <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fa fa-plus" aria-hidden="true"></i>
     </span>
-    <!-- 이벤트 v-on 등록
-    <button v-on:click="addTodo">추가</button>--> 
+
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">할 일을 입력하세요.
+        <i class="closeModalBtn fa fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
-  
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
+
 export default {
   data() {
     return {
-      newTodoItem : ''
-      }
-    }, 
-  methods : {
-    addTodo(){
-      if(this.newTodoItem !==""){
+      newTodoItem: '',
+      showModal: false
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodoItem !== "") {
         var value = this.newTodoItem && this.newTodoItem.trim();
-        localStorage.setItem(this.newTodoItem,this.newTodoItem);
-        this.clearInput;
-      }},
-      clearInput() {
-        this.newTodoItem = '';
+				this.$emit('addTodo', value)
+        this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
-      // localStorage 객체 내 setItem method는 로컬에 해당 데이터를  (key,value) 형태로 저장
-      }
+    },
+    clearInput() {
+      this.newTodoItem = '';
+    }
+  },
+  components: {
+    Modal: Modal
   }
+}
 </script>
 
-<style>
-input:focus{
-  outline : none;
+<style scoped>
+input:focus {
+  outline: none;
 }
 .inputBox {
   background: white;
   height: 50px;
   line-height: 50px;
-  border-radius : 5px;
+  border-radius: 5px;
 }
-.inputBox input{
+.inputBox input {
   border-style: none;
-  font-size : 0.9rem;
+  font-size: 0.9rem;
 }
-.addContainer{
-  float:right;
-  background: linear-gradient(to right,#6478FB,#8763FB);
-  display: block;
-  width : 3rem;
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: inline-block;
+  width: 3rem;
   border-radius: 0 5px 5px 0;
 }
-
 .addBtn {
-  color : white;
+  color: white;
   vertical-align: middle;
 }
 </style>
